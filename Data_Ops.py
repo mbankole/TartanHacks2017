f61af4b6acae28e2f4db473f5b3bbc2a3c132f96
Bolaji Bankole <renderer2@gmail.com>
2017-02-10 21:44:01 -0500

    more stuff
    so descriptive

---
+++ Data_Ops.py
@@ -0,0 +1,17 @@
+# working with the data wooooooo
+
+# def for a location
+
+location = {
+    'name':"wow a name",
+    'coords':[24324,242],
+    'adjacents':[{
+        'name':'adjacent name',
+        'distance':2525,
+    },
+    {
+        'name':'adjacent name',
+        'distance':2525,
+    },
+    ]
+}
--- Djikstra_Ops.py
+++ Djikstra_Ops.py
@@ -3,6 +3,11 @@ import math, io, ast, numpy as np
 earthrad = 2.0904e+7
 
 
+def find_path():
+    """takes in starting node, end node, """
+    pass
+
+
 def dist(p1, p2): #haversine
     cv = math.pi/180
     dN = (p1[0]-p2[0])*cv
@@ -10,4 +15,5 @@ def dist(p1, p2): #haversine
     dRo = 2*math.asin(math.sqrt((math.sin(dN/2))**2+
         math.cos(p1[0]*cv)*math.cos(p2[0]*cv)*(math.sin(dW/2))**2))
     print(dRo)
-    return dRo*earthradp
+    return dRo*earthrad
+
