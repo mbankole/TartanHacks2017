69e67600ddab50762b5d34891d58bd7a97ce45a5
Unknown <yitongl@andrew.cmu.edu>
2017-02-11 12:39:38 -0500

    form prettier
    pretty

--- static/js/input.js
+++ static/js/input.js
@@ -1,26 +1,22 @@
+var count = 3;
+function removebutton(){
+    var elem = document.getElementById('button');
+    elem.parentNode.removeChild(elem);
+}
 function addrow(){
-    var count = 3;
-    row = `<div class="inputfield col s6 13">
-                <input id="building`;
+    row = `<div class="row"><div class="inputfield col s12 l7"><input id="building`;
     row += count;
     row += `" type="text" class="validate" name=building`;
     row += count;
-    row += `>
-                <label for="building`;
+    row += `><label for="building`;
     row += count;
-    row += `">Building</label>
-            </div>
-            <div class=""inputfield col s6 13>
-                <input id="room`;
+    row += `">Building</label></div><div class="inputfield col s12 l4"><input id="room`;
     row += count;
     row += `" type="text" class="validate" name=room`;
     row += count;
-    row += `>
-                <label for="room`;
+    row += `><label for="room`;
     row += count;
-    row += `">Room</label>
-            </div>
-        </div>`;
+    row += `">Room</label></div><div id="button"><a onclick="removebutton();addrow()" class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">add</i></a></div></div>`;
     $("#newrows").append (row);
     count++;
     console.log(row);
