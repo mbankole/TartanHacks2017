defddef056f4174df9c5a2b036b8359f1371b08f
Bolaji Bankole <renderer2@gmail.com>
2017-02-10 21:32:47 -0500

    more stuff
    wow

--- Database_Ops.py
+++ Database_Ops.py
@@ -1,1 +1,29 @@
 # database operations shit goes here
+
+from __future__ import print_function
+# import mysql.connector
+# from mysql.connector import errorcode
+
+__author__ = "Bolaji Bankole"
+
+
+config = {
+  'user': 'jeff',
+  'password': 'potato salad',
+  # 'host': '10.0.0.10',
+  'host': 'antel.mbankole.com',
+  'database': 'img4',
+  'raise_on_warnings': False,
+}
+
+"""
+def init(cnx):
+    cursor = cnx.cursor()
+    query = 'CREATE TABLE IF NOT EXISTS `users` (uid INT, username VARCHAR(64), password VARCHAR(128), ' \
+            'email VARCHAR(64))'
+    cursor.execute(query)
+    query = 'CREATE TABLE IF NOT EXISTS `rooms` (room VARCHAR(64), description TINYTEXT)'
+    cursor.execute(query)
+    cursor.close()
+    cnx.commit()
+"""
--- Djikstra_Ops.py
+++ Djikstra_Ops.py
@@ -1,1 +1,13 @@
 # pathing shit here
+import math, io, ast, numpy as np
+earthrad = 2.0904e+7
+
+
+def dist(p1, p2): #haversine
+    cv = math.pi/180
+    dN = (p1[0]-p2[0])*cv
+    dW = (p1[1]-p2[1])*cv
+    dRo = 2*math.asin(math.sqrt((math.sin(dN/2))**2+
+        math.cos(p1[0]*cv)*math.cos(p2[0]*cv)*(math.sin(dW/2))**2))
+    print(dRo)
+    return dRo*earthradp
